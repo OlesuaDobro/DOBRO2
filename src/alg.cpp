294ee598248a946e1e84cae2b1ef16ab2854a571
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-03-10 17:31:53 +0300

    9

--- src/alg.cpp
+++ src/alg.cpp
@@ -37,7 +37,7 @@ double result = 0.0;
 
 double sinn(double x, uint16_t count) {
     double result = 0.0;
-    for (uint16_t n = 0; n < count; n++) {
+    for (uint16_t n = 2; n < count; n++) {
         double sign = (n % 2 == 0) ? 1.0 : -1.0;
         double item = sign * calcItem(x, 2 * n + 1);
         result += item;
@@ -47,7 +47,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
 double result = 0.0;
-    for (uint16_t n = 0; n < count; n++) {
+    for (uint16_t n = 1; n < count; n++) {
         double sign = (n % 2 == 0) ? 1.0 : -1.0;
         double item = sign * calcItem(x, 2 * n);
         result += item;
